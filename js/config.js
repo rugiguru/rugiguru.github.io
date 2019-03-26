3c92b7ff7dd211b3cae9d88c41e800fd43e6b33f
Mani Allenki <manikanta@maventus.onmicrosoft.com>
2019-03-26 11:04:56 +0530

    added

---
+++ js/config.js
@@ -0,0 +1,21 @@
+/******************************************************************************************************************/
+/* BRAX | Responsive Personal Portfolio Template                                                                  */
+/* (c) 2018 Rene Puchinger                                                                                        */
+/******************************************************************************************************************/
+/* CONFIG                                                                                                         */
+/******************************************************************************************************************/
+
+/* Edit the array as you wish. It will contain the typed strings. */
+var config_typedArray = ["I am web developer.", "I am creative."];
+
+/* Disable Parallax effect on touch devices for performance reasons. */
+var config_disableParalaxOnTouchDevices = true;
+
+/* Enable typed effect  */
+var config_typed = false;
+
+/* Google map parameters */
+var config_mapLat = 51.509036; // latitude
+var config_mapLng = -0.123280; // longitude
+var config_mapZoom = 16; // map zoom
+var config_mapTitle = 'My company' // marker title
